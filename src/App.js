37cd80e98aa5524e460506f5a7b853f3f0d0ef8f
wojciechosmulski <34412964+wojciechosmulski@users.noreply.github.com>
2023-12-11 13:47:17 +0100

    feat: react router task

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from 'react-router-dom'
+import { useLocation } from 'react-router-dom'
+
+const Home = () => <h2>Home</h2>
+const About = () => <h2>About</h2>
+const Services = () => <h2>Services</h2>
+const Contact = () => <h2>Contact</h2>
+const ContactUs = () => <h2>ContactUS</h2>
+const ContactPl = () => <h2>ContactPL</h2>
+const ContactDe = () => <h2>ContactDE</h2>
+
+function Is404() {
+  let location = useLocation()
+  console.log(location)
+  return (
+    <div>
+      <h1>Nie znaleziono elementu: {location.pathname}</h1>
+    </div>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/contact/us" element={<ContactUs />} />
+        <Route path="/contact/pl" element={<ContactPl />} />
+        <Route path="/contact/de" element={<ContactDe />} />
+        <Route path="*" element={<Is404 />} />
+      </Routes>
+    </Router>
+  )
 }
 
-export default App;
+export default App
